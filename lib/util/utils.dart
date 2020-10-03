b61db4f0b5c6e69e7ac6ca1077bbe42987b08a4b
malengatiger <malengatiger@gmail.com>
2020-10-03 22:24:55 +0200

    initial commit II

---
+++ lib/util/utils.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+const BLUE_DOT = "🔵 ";
+const BLUE_HEART = '💙 ';
+const PINK_FLOWER = '🌸 ';
+const YELLOW_FLOWER = '🌼 ';
+const FERN = '🌿 ';
+const APPLE = '🍏 ';
+const PEACH = '🍊 ';
+const PEAR = '🍐 ';
+const ERROR = '👿 ';
